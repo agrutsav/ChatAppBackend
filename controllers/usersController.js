65fbeb17a78abd5ca4fc35855e17264235debe10
Utsav Agrawal <agrutsav88728@gmail.com>
2023-07-01 19:34:23 +0530

    initial commit

---
+++ controllers/usersController.js
@@ -0,0 +1,75 @@
+const User = require("../model/userModel");
+const bcrypt = require("bcrypt");
+
+module.exports.register = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const usernameCheck = await User.findOne({ username });
+    if (usernameCheck) {
+      return res.json({ msg: "Username already taken", status: false });
+    }
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck) {
+      return res.json({ msg: "Email already used", status: false });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      email,
+      username,
+      password: hashedPassword,
+    });
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const users = await User.findOne({ username });
+    if (!users) {
+      return res.json({ msg: "Incorrect Username", status: false });
+    }
+    const isPasswordValid = await bcrypt.compare(password, users.password);
+    if (!isPasswordValid) {
+      return res.json({ msg: "Incorrect password", status: false });
+    }
+    delete users.password;
+    return res.json({ status: true, users });
+  } catch (ex) {
+    next(ex);
+  }
+};
+module.exports.setAvatar = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const avatarImage = req.body.image;
+    console.log(userId);
+    const userData = await User.findByIdAndUpdate(userId, {
+      isAvatarImageSet: true,
+      avatarImage,
+    });
+    return res.json({
+      isSet: userData.isAvatarImageSet,
+      image: userData.avatarImage,
+    });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({ _id: { $ne: req.params.id } }).select([
+      "email",
+      "username",
+      "avatarImage",
+      "_id",
+    ]);
+    return res.json(users);
+  } catch (ex) {
+    next(ex);
+  }
+};
